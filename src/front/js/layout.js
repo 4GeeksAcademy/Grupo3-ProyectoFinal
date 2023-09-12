b1763e02b856fa47511f064f245c2e988a886b75
Caro72 <106418089+Caro72@users.noreply.github.com>
2023-09-12 04:25:28 +0000

    layout2

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -10,7 +10,7 @@ import { EditProject } from "./pages/editProject";
 import { ListProject } from "./pages/listProject";
 import injectContext from "./store/appContext";
 
-//create your first component
+//create your first component here
 const Layout = () => {
     //the basename is used when your project is published in a subdirectory and not in the root of the domain
     // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
