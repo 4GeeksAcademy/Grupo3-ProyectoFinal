20df1a53300bf3e284f0acf1f296ab53290b093c
Caro72 <106418089+Caro72@users.noreply.github.com>
2023-09-12 04:17:43 +0000

    change layout

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -3,11 +3,11 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 import { BackendURL } from "./component/backendURL";
 import { Home } from "./pages/home";
+import { Login } from "./pages/Login";
 import CreateQuotation from "./pages/CreateQuotation"
 import { CreateProject } from "./pages/createProject";
 import { EditProject } from "./pages/editProject";
 import { ListProject } from "./pages/listProject";
-import { Login } from "./pages/login";
 import injectContext from "./store/appContext";
 
 //create your first component
