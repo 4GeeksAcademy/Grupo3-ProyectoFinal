7b1c966a8341dc9146e57d0b067662d784d361c8
Caro72 <106418089+Caro72@users.noreply.github.com>
2023-09-12 02:43:46 +0000

    login

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,7 +1,12 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
+			isloged: false,
 			message: null,
+			login_user: {
+				email: null,
+				password: null
+			},
 			demo: [
 				{
 					title: "FIRST",
@@ -18,46 +23,81 @@ const getState = ({ getStore, getActions, setStore }) => {
 			// quotations: []
 		},
 		actions: {
-			
+			logInUser: async () => {
+				const store = getStore()
+				const actions = getActions()
+				try {
+					if (actions.isPropertyEmpty(store.login_user)) {
+						alert("Le falta llenar algunos datos :S");
+						return;
+					}
+
+					const response = await fetch(process.env.BACKEND_URL + "/login", {
+						method: 'POST',
+						body: JSON.stringify(store.login_user),
+						headers: {
+							'Content-Type': 'application/json'
+						}
+					})
+					const result = await response.json()
+					console.log(result);
+
+					if (response.ok) {
+						localStorage.setItem("jwt-token", result.access_token);
+						alert("Login success");
+						setStore({ isloged: true });
+						return true;
+					} else {
+						setStore({ isloged: false })
+						alert(result.message)
+					}
+
+				} catch (error) {
+					console.log(error + " Error loading message from backend")
+					setStore({ isloged: false })
+				}
+
+			},
+
 			// Use getActions to call a function within a fuction
 			exampleFunction: () => {
 				getActions().changeColor(0, "green");
 			},
 
 			getMessage: async () => {
-				try{
+				try {
 					// fetching data from the backend
 					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
 					const data = await resp.json()
 					setStore({ message: data.message })
 					// don't forget to return something, that is how the async resolves
 					return data;
-				}catch(error){
+				} catch (error) {
 					console.log("Error loading message from backend", error)
 				}
 			},
 
 			postQuotationData: async (taskArray) => {
 				try {
-				  const response = await fetch(process.env.BACKEND_URL + "/api/user/client/quation/create", {
-					method: 'POST', 
-					headers: {
-					  'Content-Type': 'application/json'
-					},
-					body: JSON.stringify(taskArray) 
-				  });
-			
-				  if (!response.ok) {
-					throw new Error('Error en la petición');
-				  }
-			
-				  const responseData = await response.json(); 
-				  setData(responseData);
-				} catch (error) { 
-				  console.error('Ha habido un error:', error);
+					const response = await fetch(process.env.BACKEND_URL + "/api/user/client/quation/create", {
+						method: 'POST',
+						headers: {
+							'Content-Type': 'application/json'
+						},
+						body: JSON.stringify(taskArray)
+					});
+
+					if (!response.ok) {
+						throw new Error('Error en la petición');
+					}
+
+					const responseData = await response.json();
+					setData(responseData);
+				} catch (error) {
+					console.error('Ha habido un error:', error);
 				}
-			  
-		},
+
+			},
 
 			changeColor: (index, color) => {
 				//get the store
@@ -72,6 +112,15 @@ const getState = ({ getStore, getActions, setStore }) => {
 
 				//reset the global store
 				setStore({ demo: demo });
+			},
+
+			handleChange: (e, type) => {
+				const store = getStore()
+				if (type == "login") {
+					const newUserData = { ...store.login_user }
+					newUserData[e.target.name] = e.target.value
+					setStore({ login_user: newUserData })
+				}
 			}
 		}
 	};
