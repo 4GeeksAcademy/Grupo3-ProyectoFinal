e1ccfd70abe080c6434cf55c7aa59ee1d915b652
ElenaZu <128751514+ElenaZu@users.noreply.github.com>
2023-09-17 21:46:49 +0000

    Coreeccion de estilos app

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,14 +1,14 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import "../../styles/home.css";
+//import "../../styles/home.css";
 import { Background } from "../component/background";
-import Login from "../pages/Login.js";
+//import Login from "../pages/Login.js";
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
 		<>
-			<Login></Login>
+			<Background></Background>
 		</>
 
 	);
