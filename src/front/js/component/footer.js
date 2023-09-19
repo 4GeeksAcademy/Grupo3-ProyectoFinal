01b7048e3b4d51a1322d5be9f32c509318537b2d
Caro72 <106418089+Caro72@users.noreply.github.com>
2023-09-19 08:25:44 +0000

    add login and register css

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 export const Footer = () => (
 	<footer className='text-center text-white'>
 
-		<div className='text-center p-3' style={{ backgroundColor: 'rgba(77, 221, 173, 0.2)', color: 'black' }}>
+		<div className='text-center p-3' style={{ backgroundColor: 'white', color: 'black' }}>
 			© {new Date().getFullYear()} Copyright:
 			<a className='text-black'>
 				FreeLancify.com
